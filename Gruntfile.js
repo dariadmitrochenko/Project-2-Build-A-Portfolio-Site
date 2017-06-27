f7c59399f1f957af2d0b9bbb7d9ddcd07422ff0c
dariadmitrochenko <dmitrochenko.d@gmail.com>
2017-06-27 14:41:35 -0400

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 250,
+            suffix: '_large',
+            quality: 60
+          },{
+            width: 250,
+            suffix: '_medium',
+            quality: 50
+          },{
+            width: 250,
+            suffix: '_small',
+            quality: 40
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
+
+};
---
+++ README.md
@@ -0,0 +1,19 @@
+# Portfolio Website Project
+Responsive portfolio website.
+
+by **[Daria Dmitrochenko](https://github.com/dariadmitrochenko)**
+## Installing
+* Clone the repository from Github:
+
+ 1)clone the [repository from GitHub](https://github.com/dariadmitrochenko/Project-2-Portfolio-Website.git) (instructions on how to clone a repository from Github [here](https://help.github.com/articles/cloning-a-repository/))
+
+ 2) `cd` into the directory
+
+ 3) type `open index.html`, it should open the webpage in your default browser
+* Download a zip file
+
+ 1) unzip the file
+
+ 2) open the index.html from the folder by double-clicking on it
+
+ 3) choose `run module` command and run the program, it should open the webpage in your default browser
